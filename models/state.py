aea4a6d717feeddbf8a3118c9eb55c104947cd0c
Mulongovictor <mulongovibu@gmail.com>
2024-01-13 01:48:58 -0800

    Fields of the various data

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" Class thatrepresent the state """
+
+
+class State(BaseModel):
+
+    """
+    Initialize state classinstance
+    of basemodel class
+    """
+
+    name = ""
