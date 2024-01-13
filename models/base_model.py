a40dc78cbd7770a143e999171afe144aebf2698b
Mulongovictor <mulongovibu@gmail.com>
2024-01-13 00:24:49 -0800

    BaseModel class developed

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Definition of models module """
+"""Type module of BaseModel"""
 
 import models
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """ Defines all common attributes / methods for other classes """
 
     def __init__(self, *args, **kwargs):
@@ -20,27 +20,18 @@ class BaseModel():
                             "updated_at" : the sat which an instance is updated
         """
 
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == "id":
-                    self.id = v
-                elif k == "created_at":
-                    if isinstance(v, str):
-                        self.created_at = datetime.strptime(v, time_format)
-                    else:
-                        self.created_at = v
-                elif k == "updated_at":
-                    if isinstance(v, str):
-                        self.updated_at = datetime.strptime(v, time_format)
-                    else:
-                        self.updated_at = v
-                elif k != "__class__":
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v, timeformat))
+                elif key != '__class__':
                     setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def save(self):
         """
@@ -48,7 +39,7 @@ class BaseModel():
         the current datetime
         """
 
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
@@ -59,8 +50,16 @@ class BaseModel():
         """
 
         my_dict = self.__dict__.copy()
-        my_dict["created_at"] = self.created_ati \
-            .strftime("%Y-%m-%dT%H:%M:%S.%f")
+        my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         my_dict["__class__"] = self.__class__.__name__
-        return (my_dict)
+        return my_dict
+
+    def __str__(self):
+        """
+        When we call print function on the model,
+        this is the format of display
+        """
+
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
