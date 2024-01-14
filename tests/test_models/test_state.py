c7a6c4503d048d2b4ff316dc60f0d954ade12503
Victor <victormwendwa804@gmail.com>
2024-01-14 09:22:38 -0800

    State

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Unittest for state module
-"""
+"""State module unittest"""
 import os
 import unittest
 from models.state import State
@@ -10,41 +8,40 @@ from models.engine.file_storage import FileStorage
 
 
 class Test_State(unittest.TestCase):
-""" Test for
-State Class """
+    """State Class test"""
 
-def setUp(self):
-"""set up the
-test for testing States"""
-FileStorage._FileStorage__file_path = "test.json"
-self.state = State()
-self.state.name = "Florida"
-self.state.save()
+    def setUp(self):
+        """set, testing States"""
+        FileStorage._FileStorage__file_path = "test.json"
+        self.state = State()
+        self.state.name = "Florida"
+        self.state.save()
 
-def test_docstring_State(self):
-"""checking for docstrings"""
-self.assertIsNotNone(State.__doc__)
+    def test_docstring_State(self):
+        """Docstrings check"""
+        self.assertIsNotNone(State.__doc__)
 
-def test_instance_State(self):
-"""checking for valid type"""
-self.assertTrue(type(self.state.name) is str)
+    def test_instance_State(self):
+        """Valid type check"""
+        self.assertTrue(type(self.state.name) is str)
 
-def test_to_dict_State(self):
-"""test if dictionary works"""
-self.assertEqual('to_dict' in dir(self.state), True)
+    def test_to_dict_State(self):
+        """Dictionary works test"""
+        self.assertEqual('to_dict' in dir(self.state), True)
 
-def testpublic(self):
-self.assertEqual(str, type(State().id))
+    def testpublic(self):
+        self.assertEqual(str, type(State().id))
 
-def testHasAttributes(self):
-"""verify if attributes exist"""
-self.assertTrue(hasattr(self.state, 'name'))
-self.assertTrue(hasattr(self.state, 'id'))
-self.assertTrue(hasattr(self.state, 'created_at'))
-self.assertTrue(hasattr(self.state, 'updated_at'))
+    def testHasAttributes(self):
+        """Attributes exist, verify"""
+        self.assertTrue(hasattr(self.state, 'name'))
+        self.assertTrue(hasattr(self.state, 'id'))
+        self.assertTrue(hasattr(self.state, 'created_at'))
+        self.assertTrue(hasattr(self.state, 'updated_at'))
 
-def tearDown(self):
-os.remove(FileStorage._FileStorage__file_path)
+    def tearDown(self):
+        os.remove(FileStorage._FileStorage__file_path)
+
 
 if __name__ == "__main__":
-unittest.main()
+    unittest.main()
